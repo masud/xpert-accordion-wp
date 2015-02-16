12588550230b22060e3ca2adfa5bd06e2d307769
masud <masud.ahmed92@gmail.com>
2015-02-16 14:14:29 +0600

    gulp Error Handler pakage added

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,22 @@
 var gulp = require('gulp'),
     less = require('gulp-less');
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    plumber = require('gulp-plumber');
 
 gulp.task('compile', function(){
-	gulp.src('assets/less/**/*.less')
-		.pipe( less() )
-		.pipe( gulp.dest('assets/css') )
-		.pipe(livereload());
+  gulp.src('assets/less/**/*.less')
+    .pipe(plumber())
+    .pipe( less())
+    .pipe( gulp.dest('assets/css'))
+    .pipe(livereload());
 });
  
-/* gulp.task('styles',function(){
-   	gulp.src('assets/vendor/uikit/less/uikit.less')
-   	.pipe(less())
-   	.pipe(gulp.dest('assets/css'));
- });
 
- gulp.task('accordion', function(){
-
- 	gulp.src('assets/vendor/uikit/less/components/accordion.less')
-   	.pipe(less())
-   	.pipe(gulp.dest('assets/css'));
- });*/
 //Watch Task
 gulp.task('watch',function(){
 
-	gulp.watch('assets/less/*.less',['compile']);
-	livereload.listen();
+  gulp.watch('assets/less/*.less',['compile']);
+  livereload.listen();
 });
+
+  gulp.task('default',['compile','watch']);
